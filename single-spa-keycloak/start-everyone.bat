d1b424215ba7cb83858dd38534966df14ee392c3
Kiran Sasibhooshan <kiran.sasibhooshan@ibsplc.com>
2022-11-02 14:26:05 +0530

    mfes

---
+++ single-spa-keycloak/start-everyone.bat
@@ -0,0 +1,31 @@
+title "Starting root config and all microfrontends independently "
+
+cd root-config
+
+Start npm run start 
+
+cd ..
+
+cd login-app
+
+Start npm run start 
+
+cd ..
+
+cd home-app
+
+Start npm run start 
+
+cd ..
+
+cd myaccount-app
+
+Start npm run start 
+
+cd ..
+
+cd registration-app
+
+Start npm run start 
+
+cd ..
